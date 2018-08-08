93da41d6e28fd56dcf4a09c371ba7b308926b83e
Gregory Austin <u14039712@tuks.co.za>
2018-08-08 09:24:21 +0200

    Started logic for reading fits files without eating memory

--- fits_loader.py
+++ fits_loader.py
@@ -26,38 +26,60 @@ import os
 
 class FitsDataset(Dataset):
     """Face Landmarks dataset."""
+    # make this more efficient 
+    # be able to know all files but also 
+    # be able to index without reading every file in
 
-    def __init__(self, root_dir, transform=None):
+
+    def __init__(self, root_dir, dimensions=544, transform=None):
         
         fitsFiles = os.listdir(root_dir)
 
         
         # TODO: make it read ALL fits files, not just this one
-        tmpFile = root_dir + '/' + fitsFiles[0]
-        tmpFile = fits.getdata(tmpFile, ext=0)
+        
 
-        img_size = 544
-        tensors = []
-        prev_x = 0
-        ## CAUTION: THIS CROPS OFF SOME OF THE FITS DATA FILES 
-        for x in range(1, int(tmpFile.shape[0]/img_size)): 
-            for y in range(1, int(tmpFile.shape[1]/img_size)-1):
-                crop_image = tmpFile[prev_x:x*img_size, y*img_size:(y*img_size + img_size)]
+        # img_size = 544
+        # tensors = []
+        # prev_x = 0
+        # ## CAUTION: THIS CROPS OFF SOME OF THE FITS DATA FILES 
+        # for x in range(1, int(tmpFile.shape[0]/img_size)): 
+        #     for y in range(1, int(tmpFile.shape[1]/img_size)-1):
+        #         crop_image = tmpFile[prev_x:x*img_size, y*img_size:(y*img_size + img_size)]
                 
-                crop_image = crop_image[..., numpy.newaxis]
-                crop_image = crop_image.transpose(2, 0, 1)
-                # TODO: Give value to new dimension?? 
-               # print(crop_image.dtype)
+        #         crop_image = crop_image[..., numpy.newaxis]
+        #         crop_image = crop_image.transpose(2, 0, 1)
+        #         # TODO: Give value to new dimension?? 
+        #         # print(crop_image.dtype)
 
-                tensors.append(torch.from_numpy(crop_image))
-            prev_x = x*img_size
+        #         tensors.append(torch.from_numpy(crop_image))
+        #     prev_x = x*img_size
 
-        self.fits_file = tensors
+        self.fits_files = fitsFiles
+        self.dimensions = dimensions
         self.root_dir = root_dir
         self.transform = transform
 
     def __len__(self):
         return len(self.fits_file)
+
+    def fancyIndex(self, index):
+    	# given an index i
+    	# given x*x for crop image dimensions 
+    	# given n files of known dimensions
+    	# i * x/2 = pixel_value (divided by 2 because of horizontal interleaving) 
+    	# pixel_value/width = rows remainder pixels
+    	# fits_rows / rows = file_number remainder rows 
+    	# there is vertical interleaving and horizontal interleaving 
+    	# 						/2 on both  
+    	pixels = index * self.dimensions/2
+    	width = 
+    	row = pixels/width
+
+
+
+    	tmpFile = root_dir + '/' + fitsFiles[0]
+        tmpFile = fits.getdata(tmpFile, ext=0)
 
     def __getitem__(self, idx):
         sample = self.fits_file[idx]
